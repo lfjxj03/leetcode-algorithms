961f708d00d3eea5acd6ae40b4917453d475fbf7
lfjxj03 <41408705+lfjxj03@users.noreply.github.com>
2018-07-22 09:12:30 +0800

    327

---
+++ count_of_range_sum.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def countRangeSum(self, nums, lower, upper):
+        """
+        :type nums: List[int]
+        :type lower: int
+        :type upper: int
+        :rtype: int
+        """
