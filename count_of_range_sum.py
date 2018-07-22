07b6bfc3e84759e99c5edcf699c3a8246bd6b616
lfjxj03 <41408705+lfjxj03@users.noreply.github.com>
2018-07-22 12:26:52 +0800

    327

--- count_of_range_sum.py
+++ count_of_range_sum.py
@@ -6,5 +6,26 @@ class Solution(object):
         :type upper: int
         :rtype: int
         """
-        def sss():
-            pass
+        l = len(nums)
+        count = 0
+        summ = [[0 for i in range(l)] for j in range(l)]
+        tags = [[False for i in range(l)] for j in range(l)]
+        for i in range(l):
+            summ[i][i] = nums[i]
+            if  nums[i] >= lower and nums[i] <= upper:
+                    tags[i][i] = True
+                    count = count + 1
+
+        for x in range(1,l):
+            for i in range(0,l-x):
+                j = i + x
+                summ[i][j]=summ[i][j-1] + nums[j]
+                if summ[i][j]>=lower and summ[i][j]<=upper:
+                    tags[i][j] = True
+                    count = count + 1
+        return count
+
+
+if __name__ == '__main__':
+    a = Solution()
+    print(a.countRangeSum([-1,3,-2,5,7,2],2,3))
